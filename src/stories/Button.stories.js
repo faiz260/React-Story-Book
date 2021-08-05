b726d3d244c5c187bbf23dd831f69e50e9118155
faiz <razafaiz815@gmail.com>
2021-08-05 18:23:37 +0500

    netlify toml file added

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Button from "../Components/Button";
 
-export default{
-    title: 'Button Story',
-    component: Button
+export default {
+  title: "Button Story",
+  component: Button,
 };
 
 export function ButtonStory() {
-  return <div>
-      <Button onClick={()=>alert("Pressed!")} value="Click me!" />
-  </div>;
+  return (
+    <div>
+      <Button onClick={() => alert("Pressed!")} value="Click me!" />
+    </div>
+  );
 }
