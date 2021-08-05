92d088d3c5099f4682769684211771b359dc6907
faiz <razafaiz815@gmail.com>
2021-08-06 00:50:23 +0500

    data fetched from server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [val, setVal] = useState({});
+
+  useEffect(() => {
+    fetch(`/.netlify/functions/hello?name=faiz`)
+      .then((res) => res.json())
+      .then((obj) => setVal(obj));
+  }, []);
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <h3>{val.message}</h3>
     </div>
   );
 }
